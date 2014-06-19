9f7f172cbe9464021b0acf6d116e633093304a57
sabusa <sabusa@shaw.ca>
2014-06-18 17:13:51 -0700

    Wow Midterm

---
+++ wow/src/ca/bcit/comp2613/a00192788/wow/DontHaveTheExpansionException.java
@@ -0,0 +1,27 @@
+package ca.bcit.comp2613.a00192788.wow;
+
+@SuppressWarnings("serial")
+public class DontHaveTheExpansionException extends Exception {
+
+	public DontHaveTheExpansionException() {
+		super();
+	}
+
+	public DontHaveTheExpansionException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public DontHaveTheExpansionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DontHaveTheExpansionException(String message) {
+		super(message);
+	}
+
+	public DontHaveTheExpansionException(Throwable cause) {
+		super(cause);
+	}
+
+}
---
+++ wow/src/ca/bcit/comp2613/a00192788/wow/Race.java
@@ -0,0 +1,7 @@
+package ca.bcit.comp2613.a00192788.wow;
+
+public enum Race {
+	WORGEN, DRAENEI, DWARF, GNOME, ORC, PANDAREN, HUMAN
+	// HUMAN Race added
+}
+
